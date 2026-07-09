2e14531e34c404856be0d1920a5b07778f244a05
Boef <boefma@celsus-fe-g03.lacdr.leidenuniv.nl>
2026-07-09 16:45:08 +0200

    refactoring, reworking of data gathering etc.

--- src/auxiliary_ranking/rank_loss.py
+++ src/auxiliary_ranking/rank_loss.py
@@ -113,9 +113,8 @@ class MSEPlusPairwiseRankingLoss(metrics.ChempropMetric):
                 print('pair loss produced NaN')
                 return mse_loss
             else:
-                with open('loss_log_.txt', 'a') as f:
-                    f.write(f'mse_loss: {mse_loss.item():.6f}, pair_loss: {pair_loss_.item():.6f}\n')
-                # print(f'mse + pair: {mse_loss + pair_loss_}')
+                # with open('loss_log_.txt', 'a') as f:
+                #     f.write(f'mse_loss: {mse_loss.item():.6f}, pair_loss: {pair_loss_.item():.6f}\n')
                 return mse_loss + pair_loss_
         else:
             print(f'issue calculating pair loss')
