88cf759a63960b7cfe7b6ed1ef07a663626dfca3
boefma <boefma@vuw.leidenuniv.nl>
2026-08-06 14:05:37 +0200

    changes to training script

--- run_pw_ranked_chemprop.py
+++ run_pw_ranked_chemprop.py
@@ -151,3 +151,4 @@ if __name__ == '__main__':
     for seed in range(10):
         seed = seed+1
         run_model(dataset_dir=current_dir / 'data/datasets/', current_dir=current_dir, protein_descriptor='Z-scales', extension='cluster_split_ext_rank_cont_set', seed=seed) 
+ 
